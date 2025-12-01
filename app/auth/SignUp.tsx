20f69b2192134adbc5733821fc411af8bb058735
Layammieh <layamgracielle@gmail.com>
2025-12-01 14:11:48 +0800

    sign up fixed

--- app/auth/SignUp.tsx
+++ app/auth/SignUp.tsx
@@ -71,7 +71,8 @@ export default function SignUpPage() {
         await AsyncStorage.setItem('user', JSON.stringify(data.user));
 
         Alert.alert('Success', 'Account created successfully!');
-        router.push(data.user.role === 'admin' ? '/admin/AdminDashboard' : '/user/UserDashboard');
+        router.push('/auth/Login');
+
       } else {
         Alert.alert('Error', data.message || 'Signup failed.');
       }
