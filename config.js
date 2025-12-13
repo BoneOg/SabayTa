b8452238e21de5c8cb2989967dd68be6d5bb35d3
BoneOg <vaughnpereyra098@gmail.com>
2025-12-13 08:06:02 +0800

    small fix

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = (process.env.EXPO_PUBLIC_API_URL || "http://192.168.1.13:3001").replace(/\/$/, "");
+export const BASE_URL = (process.env.EXPO_PUBLIC_API_URL || "http://192.168.1.16:3001").replace(/\/$/, "");
 
 //change it to your ip addressguys
 // mieh: 192.168.1.52
